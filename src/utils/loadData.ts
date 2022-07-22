95cc25d7ea667d80d1586a721fa70f8d5c8b10be
Bhawick Jain <bhawick@gmail.com>
2022-07-22 09:52:45 +0100

    adjust tests for real data read, adjust imports for data and adjust css to flex item row-wise and wrap

--- src/utils/loadData.ts
+++ src/utils/loadData.ts
@@ -1,5 +1,5 @@
 import BabyName from "../types/BabyNames";
-import babyNamesJSON from "../data/babyNamesData.json"
+import babyNamesJSON from "../data/babyNamesData.json";
 
-export const loadData = (): BabyName[] => babyNamesJSON
+export const loadData = (): BabyName[] => babyNamesJSON;
 export default loadData;
