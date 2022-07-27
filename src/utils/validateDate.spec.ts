6071938bdd69a35387f8d2e49922cff21b6567e2
Bhawick Jain <bhawick@gmail.com>
2022-07-27 15:51:36 +0100

    native implementation of data validation without widening babyName type

---
+++ src/utils/validateDate.spec.ts
@@ -0,0 +1,40 @@
+import BabyName from "../types/BabyNames";
+import validateData from "./validateDate";
+
+test("dummy test", () => {
+  const jsonData = [
+    {
+      id: 0,
+      name: "Zahra",
+      sex: "f",
+    },
+    {
+      id: 1,
+      name: "Parsa",
+      sex: "m",
+    },
+    {
+      id: 2,
+      name: "Avah",
+      sex: "f",
+    },
+  ];
+  const babyData: BabyName[] = [
+    {
+      id: 0,
+      name: "Zahra",
+      sex: "f",
+    },
+    {
+      id: 1,
+      name: "Parsa",
+      sex: "m",
+    },
+    {
+      id: 2,
+      name: "Avah",
+      sex: "f",
+    },
+  ];
+  expect(validateData(jsonData)).toStrictEqual(babyData);
+});
