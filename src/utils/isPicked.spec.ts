fb09b67d00cdbc65873f47db355f51469b91d882
Bhawick Jain <bhawick@gmail.com>
2022-07-27 08:04:28 +0100

    show pciked baies in babyDisplay list

---
+++ src/utils/isPicked.spec.ts
@@ -0,0 +1,32 @@
+import BabyName from "../types/BabyNames";
+import isPicked from "./isPicked";
+
+const initialPickState: BabyName[] = [
+  {
+    id: -1,
+    name: "bhawick",
+    sex: "m",
+  },
+  {
+    id: 0,
+    name: "sevgi",
+    sex: "f",
+  },
+  {
+    id: 2,
+    name: "lui",
+    sex: "f",
+  }
+];
+
+test("isPicked returns true when baby is present in pickedBabies", () => {
+
+  const baby: BabyName = {
+    id: 2,
+    name: "lui",
+    sex: "f",
+  };
+  const expectedResult: boolean = true
+
+  expect(isPicked(baby, initialPickState)).toEqual(expectedResult);
+});
